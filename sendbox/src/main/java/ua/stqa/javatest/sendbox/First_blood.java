78737aa8638656d4984b1c13c07b9f5d41f2353b
EgeVika <owibka86@mail.ru>
2016-02-25 00:05:22 +0200

    изменены настройки

--- sendbox/src/main/java/ua/stqa/javatest/sendbox/First_blood.java
+++ sendbox/src/main/java/ua/stqa/javatest/sendbox/First_blood.java
@@ -1,8 +1,8 @@
 package ua.stqa.javatest.sendbox;
 
 public class First_blood {
-public static void main (String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello world!");
-}
+        System.out.println("Hello world!");
+    }
 }
